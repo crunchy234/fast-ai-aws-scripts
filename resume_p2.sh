284255727aad29cc97c78b519344c3f2d1ace8ed
Joshua Cranch <crunchy234@gmail.com>
2018-02-06 12:24:11 +1300

    Fix get current dir error in resume script

--- resume_p2.sh
+++ resume_p2.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 instanceId=$1
 
+# Get this files directory
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+
 aws ec2 start-instances --instance-ids ${instanceId}
 export instanceUrl=`aws ec2 describe-instances --instance-ids ${instanceId} --query 'Reservations[0].Instances[0].PublicDnsName' --output text`
 
